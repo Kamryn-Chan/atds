e8391c0bd2e227ac5c6127d9f7e34b3a1a4c4aa8
Kamryn Chan <kchan24@students.polytechnic.org>
2024-02-23 12:22:51 -0800

    Added Deque class and removed duplicate Stack and Queue classes

--- atds.py
+++ atds.py
@@ -56,64 +56,6 @@ class Queue():
     
     def peek(self):
         if len(self.queue) > 0:
-            return self.queue[-1#!/usr/bin/env python3
-
-"""
-atds.py
-Contains data structures used in the Advanced Topics CS class.
-"""
-
-__author__ = "Kamryn Chan"
-__version__ = "2024-02-15"
-
-class Stack():
-    """Models a stack using lists
-    """
-    def __init__(self):
-        self.stack = []
-
-    def push(self, item):
-        self.stack.append(item)
-
-    def pop(self):
-        if len(self.stack) > 0:
-            return self.stack.pop()
-        else:
-            return None
-
-    def peek(self):
-        """Returns the item on the top of the stack, unless nothing is 
-there
-        """
-        if len(self.stack) > 0:
-            return self.stack[-1]
-        else:
-            return None
-
-    def size(self):
-        return len(self.stack)
-
-    def is_empty(self):
-        return len(self.stack) == 0
-
-    def __repr__(self):
-        return str(self.stack)
-
-
-class Queue():
-    """Models a queue using lists
-    """
-    def __init__(self):
-        self.queue = []
-
-    def enqueue(self, item):
-        self.queue.append(item)
-
-    def dequeue(self):
-        return self.queue.pop(0)
-    
-    def peek(self):
-        if len(self.queue) > 0:
             return self.queue[-1]
         else:
             return None
@@ -126,3 +68,32 @@ class Queue():
 
     def __repr__(self):
         return str(self.queue)
+
+class Deque(object):
+    def __init__(self):
+        self.deque = []
+    
+    def add_front(self, item):
+        self.deque.insert(0, item)
+    
+    def add_rear(self, item):
+        self.deque.append(item)
+
+    def remove_front(self):
+        return self.deque.pop(0)
+
+    def remove_rear(self):
+        return self.deque.pop()
+
+    def size(self):
+        return len(self.deque)
+
+    def is_empty(self):
+        return len(self.deque) == 0
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
