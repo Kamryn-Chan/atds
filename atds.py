a3620720e7f217c719de075c009eebcae5b253a7
Kamryn Chan <kchan24@students.polytechnic.org>
2024-04-05 09:06:48 -0700

    Added the BinaryTree class

--- atds.py
+++ atds.py
@@ -334,8 +334,85 @@ class HashTable():
         """
         return "Keys:   " + str(self.slots) + "\n" + "Values: " + str(self.data)
 
+class BinaryTree(object):
+    """Constructs a binary tree.
+    """
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+    def get_root_val(self):
+        return self.val
+
+    def set_root_val(self, new_val):
+        self.val = new_val
+    
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+
+    def insert_left(self, val):
+        new_left = BinaryTree(val)
+        new_left.left = self.left
+        self.left = new_left
+
+    def insert_right(self, val):
+        new_right = BinaryTree(val)
+        new_right.right = self.right
+        self.right = new_right
+
+    def __str__(self):
+        return "BinaryTree[key=" + str(self.val) + ", left_child=" + str(self.left) + ", right_child=" + str(self.right) + "]"
+
 def main():
-    pass
+    print("Testing the binary_tree_class file!")
+    bt = BinaryTree(3)
+    print("Instruction: bt = BinaryTree(3)")
+    print("Result:", bt)
+    print("Expect: BinaryTree[key=3,left_child=None,right_child=None]")
+    print()
+    bt.insert_left(4)
+    print("Instruction: bt.insert_left(4)")
+    print("Result:", bt)
+    print("Expect: BinaryTree[key=3,left_child=BinaryTree[key=4,left_child=None,right_child=None],right_child=None]")
+    print()
+    bt.insert_left(5)
+    print("Instruction: bt.insert_left(5)")
+    print("Result:", bt)
+    print("Expect: BinaryTree[key=3,left_child=BinaryTree[key=5,left_child=BinaryTree[key=4,left_child=None,right_child=None],right_child=None],right_child=None]")
+    print()
+    bt.insert_right(6)
+    print("Instruction: bt.insert_right(6)")
+    print("Result:", bt)
+    print("Expect: BinaryTree[key=3,left_child=BinaryTree[key=5,left_child=BinaryTree[key=4,left_child=None,right_child=None],right_child=None],right_child=BinaryTree[key=6,left_child=None,right_child=None]]")
+    print()
+    bt.insert_right(7)
+    print("Instruction: bt.insert_right(7)")
+    print("Result:", bt) 
+    print("Expect: BinaryTree[key=3,left_child=BinaryTree[key=5,left_child=BinaryTree[key=4,left_child=None,right_child=None],right_child=None],right_child=BinaryTree[key=7,left_child=None,right_child=BinaryTree[key=6,left_child=None,right_child=None]]]")
+    print()
+    l = bt.get_left_child()
+    print("Instruction: l = bt.get_left_child()")
+    print("Result: l =", l)
+    print("Expect: l = BinaryTree[key=5,left_child=BinaryTree[key=4,left_child=None,right_child=None],right_child=None]")
+    print()
+    l.set_root_val(9)
+    print("Instruction: l.set_root_val(9)")
+    print("Result: l =", l)
+    print("Expect: l = BinaryTree[key=9,left_child=BinaryTree[key=4,left_child=None,right_child=None],right_child=None]")
+    print()
+    l.insert_left(11)
+    print("Instruction: l.insert_left(11)")
+    print("Result:", bt)
+    print("Expect: BinaryTree[key=3,left_child=BinaryTree[key=9,left_child=BinaryTree[key=11,left_child=BinaryTree[key=4,left_child=None,right_child=None],right_child=None],right_child=None],right_child=BinaryTree[key=7,left_child=None,right_child=BinaryTree[key=6,left_child=None,right_child=None]]]")
+    print()
+    print("Instruction: print(bt.get_right_child().get_right_child())")
+    print("Result:", bt.get_right_child().get_right_child())
+    print("Expect: BinaryTree[key=6,left_child=None,right_child=None]")
+
 
 if __name__ == "__main__":
     main()
